01fca2fec9efc526b599337913c553648ed7e17d
wonseo <dnjstj99@naver.com>
2022-09-28 16:39:54 +0900

    week4_원서

--- week_4/원서/week4_2.c
+++ week_4/원서/week4_2.c
@@ -1,78 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 int H[100], n = 0;
+void inPlaceHeapsort();
 void downHeap();
 void printArray();
-void upHeap();
-void insertItem();
 void buildHeap();
-void inPlaceHeapSort();
+void insertItem();
+void upHeap();
 int main() {
-	int keycnt, key;
+	int keycnt, input;
 	scanf("%d", &keycnt);
-	for (int i = 0; i < keycnt; i++) {
-		scanf("%d", &key);
-		insertItem(key);
+	for (int i = 1; i <= keycnt; i++) {
+		scanf("%d", &input);
+		insertItem(input);
+
 	}
 	buildHeap();
-	inPlaceHeapSort();
+	inPlaceHeapsort();
 	printArray();
 }
-void inPlaceHeapSort() {
-	int tmp, N = n;
+void inPlaceHeapsort() {
+	int N = n, tmp;
 	while (N > 1) {
-		tmp = H[1];
-		H[1] = H[N];
-		H[N] = tmp;
+		tmp = H[N];
+		H[N] = H[1];
+		H[1] = tmp;
+
 		N--;
-
-
 		downHeap(1, N);
 	}
-
-
 }
-void downHeap(int i, int size) {
-	int tmp, bigger;
-	if (i * 2 > size) return;
-	bigger = i * 2;
-	if (H[i * 2] < H[i * 2 + 1] && i * 2 + 1 <= size) bigger = i * 2 + 1;
-	if (H[bigger] < H[i]) return;
 
-	tmp = H[bigger];
-	H[bigger] = H[i];
-	H[i] = tmp;
-
-	downHeap(bigger, size);
-}
-void upHeap(int i) {
-	int tmp;
-	if (i == 1) return;
-	if (H[i / 2] > H[i]) return;
-
-	tmp = H[i / 2];
-	H[i / 2] = H[i];
-	H[i] = tmp;
-	upHeap(i / 2);
-}
 void insertItem(int key) {
 	n++;
 	H[n] = key;
 	upHeap(n);
 }
-void printArray() {
-	for (int i = 1; i <= n; i++) printf(" %d", H[i]);
-	printf("\n");
+void downHeap(int i, int last) {
+	int tmp, bigger;
+	if (i * 2 > last) return;
+	bigger = i * 2;
+	if (H[i * 2] < H[i * 2 + 1] && i * 2 + 1 <= last) bigger = i * 2 + 1;// not = -> score 70
+	if (H[bigger] < H[i]) return;
+
+	tmp = H[i];
+	H[i] = H[bigger];
+	H[bigger] = tmp;
+
+	downHeap(bigger, last);
 }
-
-
+void upHeap(int i) {
+	int tmp;
+	if (i == 1) return;
+	if (H[i / 2] > H[i]) return;
+	tmp = H[i];
+	H[i] = H[i / 2];
+	H[i / 2] = tmp;
+	upHeap(i / 2);
+}
+void printArray() {
+	for (int i = 1; i <= n; i++) { printf(" %d", H[i]); }
+}
 void buildHeap() {
 	for (int i = n / 2; i > 0; i--) {
 		downHeap(i, n);
 	}
 }
-
-/*
-6
-24 17 33 50 60 70
-*/
